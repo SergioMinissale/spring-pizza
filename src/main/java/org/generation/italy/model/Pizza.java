11d7d554b48b617383e05791c5aaf782f92c0819
Sergio Minissale <minissale.sergio@outlook.itm>
2022-01-10 14:56:52 +0100

    Svolto Setup

---
+++ src/main/java/org/generation/italy/model/Pizza.java
@@ -0,0 +1,72 @@
+package org.generation.italy.model;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import javax.validation.constraints.NotNull;
+
+@Entity
+public class Pizza {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+
+	@NotNull
+	private String name;
+
+	@NotNull
+	private String description;
+
+	@NotNull
+	private BigDecimal price;
+
+	@ManyToMany
+	private List<Ingredient> ingredients;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public List<Ingredient> getIngredients() {
+		return ingredients;
+	}
+
+	public void setIngredients(List<Ingredient> ingredients) {
+		this.ingredients = ingredients;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public BigDecimal getPrice() {
+		return price;
+	}
+
+	public void setPrice(BigDecimal price) {
+		this.price = price;
+	}
+
+}
